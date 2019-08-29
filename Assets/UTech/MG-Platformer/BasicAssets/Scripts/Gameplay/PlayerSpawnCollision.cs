eec0d17c9d1e098460b47dde9e90532f16992c48
João Paulo Lins <joaopaulolinsoliveira@gmail.com>
2019-08-29 12:31:25 -0300

    Checkpoints
    - Criei PlayerSpawnCollision para registrar um novo spawPoint quando o jogador passar pelo checkpoint
    - Alterei o SpawnPoint para identificar que o jogador passou pelo checkpoint e registrar a nova posição de respaw quando ele morrer

---
+++ Assets/UTech/MG-Platformer/BasicAssets/Scripts/Gameplay/PlayerSpawnCollision.cs
@@ -0,0 +1,21 @@
+﻿using Platformer.Core;
+using Platformer.Mechanics;
+using Platformer.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer.Gameplay
+{
+    public class PlayerSpawnCollision : Simulation.Event<PlayerSpawnCollision>
+    {
+        public SpawnPoint spawnPoint;
+
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        public override void Execute()
+        {
+            model.spawnPoint = spawnPoint.transform;
+        }
+    }
+}
--- Assets/UTech/MG-Platformer/BasicAssets/Scripts/Mechanics/SpawnPoint.cs
+++ Assets/UTech/MG-Platformer/BasicAssets/Scripts/Mechanics/SpawnPoint.cs
@@ -1,12 +1,36 @@
+using Platformer.Gameplay;
 using UnityEngine;
+
+using static Platformer.Core.Simulation;
 
 namespace Platformer.Mechanics
 {
     /// <summary>
     /// Marks a gameobject as a spawnpoint in a scene.
     /// </summary>
+    /// 
+    [RequireComponent(typeof(BoxCollider2D))]
     public class SpawnPoint : MonoBehaviour
     {
+        private BoxCollider2D boxCollider;
 
+        private void Start()
+        {
+            boxCollider = GetComponent<BoxCollider2D>();
+            boxCollider.isTrigger = true;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var player = other.gameObject.GetComponent<PlayerController>();
+            if (player != null) OnPlayerEnter();
+        }
+
+        private void OnPlayerEnter()
+        {
+            //send an event into the gameplay system to perform some behaviour.
+            var ev = Schedule<PlayerSpawnCollision>();
+            ev.spawnPoint = this;
+        }
     }
 }
